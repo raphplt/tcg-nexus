c98eae5e3ad5c55638e265e3ef8393a97b1b65d8
raphplt <raphael.plassart@gmail.com>
2026-05-15 15:20:59 +0200

    feat: add Docusaurus documentation service with Nginx configuration and Docker setup

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "http://localhost:3000",
+  url: "https://docs.tcg-nexus.org",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
